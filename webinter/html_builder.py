4f80c60146f6fa25c8be2030d16e851c2cea7364
IcyPebble <100799513+IcyPebble@users.noreply.github.com>
2025-02-19 19:43:37 +0100

    Replace match statement

--- webinter/html_builder.py
+++ webinter/html_builder.py
@@ -73,59 +73,61 @@ class HTMLBuilder:
         
         return str(a)
 
+    def _img(id, **attr):
+            a = Airium()
+            with a.div():
+                a.img(id=id, **attr)
+            return str(a)
+            
+    def _audio(id, **attr):
+            a = Airium()
+            with a.div():
+                a.audio(id=id, **attr)
+            return str(a)
+
+    def _video(id, **attr):
+            a = Airium()
+            with a.div():
+                a.video(id=id, **attr)
+            return str(a)
+
+    def _p(id, **attr):
+            a = Airium()
+            text = attr.pop("text", "")
+
+            with a.div():
+                a.p(id=id, _t=text)
+            return str(a)
+            
+    def _h(id, **attr):
+            a = Airium()
+            text = attr.pop("text", "")
+            size = attr.pop("size", 1)
+
+            with a.div():
+                getattr(a, f"h{size}")(id=id, _t=text)
+            return str(a)
+
     @classmethod
     def create_html_element(this, tag_name, id, **attr):
         if attr.get("type", 0) is None: attr.pop("type")
         attr.pop("style", None)
-        match tag_name:
-            case "input":
-                match attr["type"]:
-                    case "number" | "text" | "color" | "file" | "checkbox" | "range":
-                        return this._simple_input(id, **attr)
+        tag_name = tag_name.replace("-", "_")
+
+        if tag_name == "input":
+            if attr["type"] in ["number", "text", "color", "file", "checkbox", "range"]:
+                return this._simple_input(id, **attr)
                     
-                    case "button" | "select" | "textarea":
-                        return this.__getattribute__(this, f"_{attr['type']}")(id, **attr)
+            elif attr["type"] in ["button", "select", "textarea"]:
+                return this.__getattribute__(this, f"_{attr['type']}")(id, **attr)
                     
-                    case _:
-                        raise ValueError(f"\'{attr['type']}\' is not a supported input type")
+            else:
+                raise ValueError(f"\'{attr['type']}\' is not a supported input type")
             
-            case "drawing-board":
-                return this._drawing_board(id, **attr)
-
-            case "img":
-                a = Airium()
-                with a.div():
-                    a.img(id=id, **attr)
-                return str(a)
-            
-            case "audio":
-                a = Airium()
-                with a.div():
-                    a.audio(id=id, **attr)
-                return str(a)
-
-            case "video":
-                a = Airium()
-                with a.div():
-                    a.video(id=id, **attr)
-                return str(a)
-
-            case "p":
-                a = Airium()
-                text = attr.pop("text", "")
-
-                with a.div():
-                    a.p(id=id, _t=text)
-                return str(a)
-            
-            case "h":
-                a = Airium()
-                text = attr.pop("text", "")
-                size = attr.pop("size", 1)
-
-                with a.div():
-                    getattr(a, f"h{size}")(id=id, _t=text)
-                return str(a)
-
-            case _:
+        else:
+            try:
+                return this.__getattribute__(
+                    this, f"_{tag_name}"
+                )(id, **attr)
+            except AttributeError:
                 raise NotImplementedError(f"tag_name = '{tag_name}'")
